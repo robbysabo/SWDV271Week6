75bb4e9de7627646886e63eb236d875eb8d789d8
Robby Sabo <robertalensabo@gmail.com>
2024-02-23 09:42:04 -0700

    Init Upload

---
+++ Hour 1 TIY/Assets/Notes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    /*
+     
+    I did Chapter 1, Chapter 2, Chapter 3, and Chapter 4 in this project.
+
+     */
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+I did Chapter 1-4 in Hour 1 DIY.
+
+Each scene has the corresponding name of Chapter, DIY, and Exercise.
